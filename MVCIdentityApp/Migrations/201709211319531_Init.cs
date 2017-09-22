ec252555f7efa99a0343388b7fe1ffc6d34eed90
Brighty <abright2012@gmail.com>
2017-09-22 17:19:45 +0100

    Add project files.

---
+++ MVCIdentityApp/Migrations/201709211319531_Init.cs
@@ -0,0 +1,20 @@
+namespace MVCIdentityApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Init : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Town", c => c.String());
+            AddColumn("dbo.AspNetUsers", "BirthDate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "BirthDate");
+            DropColumn("dbo.AspNetUsers", "Town");
+        }
+    }
+}
